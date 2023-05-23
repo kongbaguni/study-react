f6cc96165b3f22388fc97eacd289f1371269d2bc
kitri <student@kitri.co.kr>
2023-05-23 10:16:17 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+function App() {
+
+  return (
+    <div className='app'>
+      <h1>react test</h1>
+      <hr/>
+      aaa
+    </div>
+  );
+}
+
+export default App;
